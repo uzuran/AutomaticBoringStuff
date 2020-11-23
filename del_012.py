e2a42474eb65e463713d40662014818d2a916392
Artom Cernopascenko <archloverlinux@gmail.com>
2020-11-23 03:46:37 +0100

    add some trick

---
+++ del_012.py
@@ -0,0 +1,15 @@
+name = 'Zhopie a cat'
+newName = name[0:7] + 'the' + name[8:12]
+print(newName)
+
+eggs = [1, 2, 3]
+
+del eggs[2]
+del eggs[1]
+del eggs[0]
+
+eggs.append(4)
+eggs.append(5)
+eggs.append(6)
+
+print(eggs)
